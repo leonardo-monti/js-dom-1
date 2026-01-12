c1dd940a465e8a5351ec368b3b7d09b6147588ce
leonardo-monti <monti.leonardo22@gmail.com>
2026-01-12 17:54:23 +0100

    end fix

--- main.js
+++ main.js
@@ -5,10 +5,10 @@ const bottone = document.getElementById(`btn`)
 bottone.addEventListener(`click`, function () {
 
     if (lampadina.src.includes(`white`)) {
-        lampadina.src = `yellow_lamp.png`;
+        lampadina.src = `./img/yellow_lamp.png`;
         bottone.textContent = `Spegni`;
     } else {
-        lampadina.src = `white_lamp.png`;
+        lampadina.src = `./img/white_lamp.png`;
         bottone.textContent = `Accendi`
     }
 }) 

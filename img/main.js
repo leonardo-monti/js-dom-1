120d0f7c924857553e357e5628c629ae2b2d0b34
leonardo-monti <monti.leonardo22@gmail.com>
2026-01-12 16:41:57 +0100

    end bonus

--- img/main.js
+++ img/main.js
@@ -3,5 +3,12 @@ const lampadina = document.getElementById(`lampadina`)
 const bottone = document.getElementById(`btn`)
 
 bottone.addEventListener(`click`, function () {
-    lampadina.src = `yellow_lamp.png`
-})
+
+    if (lampadina.src.includes(`white`)) {
+        lampadina.src = `yellow_lamp.png`;
+        bottone.textContent = `Spegni`;
+    } else {
+        lampadina.src = `white_lamp.png`;
+        bottone.textContent = `Accendi`
+    }
+}) 

6d38a9823bdc096c3a3a6e8957e984124d820726
leonardo-monti <monti.leonardo22@gmail.com>
2026-01-12 16:18:44 +0100

    end

--- img/main.js
+++ img/main.js
@@ -1,3 +1,7 @@
 const lampadina = document.getElementById(`lampadina`)
 
 const bottone = document.getElementById(`btn`)
+
+bottone.addEventListener(`click`, function () {
+    lampadina.src = `yellow_lamp.png`
+})
